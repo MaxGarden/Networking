07476ce9e08300d54342498db990d4960a2a6c89
Mateusz Serkies <mateusz.serkies@artifexmundi.com>
2019-11-03 12:45:51 +0100

    Added virtual destructor to ConnectionInternal.

--- Source/Networking/ConnectionInternal.h
+++ Source/Networking/ConnectionInternal.h
@@ -6,6 +6,8 @@ namespace Networking
     class IConnectionInternal : public IConnection
     {
     public:
+        virtual ~IConnectionInternal() override = default;
+
         virtual const ConnectionID& GetID() const noexcept = 0;
 
         virtual void OnReceived(const Payload& data) = 0;
